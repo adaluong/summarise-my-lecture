e351f9d2f17f2e7c39eccd13daae96eb5fcef481
Angeni Bai <angenibai@gmail.com>
2021-09-04 16:21:03 +1000

    input validation for youtube links

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,27 +2,27 @@ import './Search.css';
 import React, { useState } from 'react';
 import { Form, Button, Card, Nav } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import { Formik } from 'formik';
 
 const Search = () => {
   const history = useHistory();
-  const [linkInput, setLinkInput] = useState("");
   const[activeTab, setActiveTab] = useState("youtube");
 
-  const onChange = (e) => {
-    e.preventDefault();
-    setLinkInput(e.target.value);
-  }
-
-  const onSubmit = (youtubeLink) => {
+  // parses link for video id
+  // returns null if invalid id
+  const parseLink = (inputLink) => {
     // regex to parse video id from
     // https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
     const reg = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    const match = youtubeLink.match(reg);
+    const match = inputLink.match(reg);
     if (!match || match[7].length !== 11) {
-      // do nothing i guess
-      return;
+      return null;
     }
-    const videoId = match[7];
+    return match[7];
+  }
+
+  const submitLink = (inputLink) => {
+    const videoId = parseLink(inputLink);
     history.push(`/result/${videoId}`);
   }
 
@@ -47,17 +47,45 @@ const Search = () => {
           </Nav>
         </Card.Header>
         <Card.Body className="formCardBody">
-          {activeTab === "youtube" &&(
-            <Form name="youtubeForm" onSubmit={() => onSubmit(linkInput)}>
-              <Form.Group className="mb-3" controlId="formVideoLink">
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Youtube link"
-                  onChange={onChange}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">Summarise!</Button>
-            </Form>)}
+          {activeTab === "youtube" && (
+            <Formik
+              initialValues={{ youtubeLink: '' }}
+              validate={values => {
+                const errors = {};
+                if (!values.youtubeLink || !parseLink(values.youtubeLink)) {
+                  errors.youtubeLink = "Enter valid Youtube link"
+                }
+                return errors;
+              }}
+              onSubmit={(values) => {
+                submitLink(values.youtubeLink);
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                values,
+                errors,
+              }) => (
+                <Form noValidate name="youtubeForm" onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3" controlId="formVideoLink">
+                    <Form.Control
+                      type="text"
+                      name="youtubeLink"
+                      value={values.youtubeLink}
+                      onChange={handleChange}
+                      isInvalid={!!errors.youtubeLink}
+                      placeholder="Enter a Youtube link"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.youtubeLink}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">Summarise!</Button>
+                </Form>
+              )}
+            </Formik>
+            )}
           {activeTab === "upload" && (
             <Form name="uploadForm">
               <Form.Group controlId="uploadTranscript" className="mb-3">
