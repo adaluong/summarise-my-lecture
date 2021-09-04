b985ee09a15e63d83b7246ed5b2771d57d75611c
Angeni Bai <angenibai@gmail.com>
2021-09-04 21:06:04 +1000

    error handling for video titles, transcripts and chats

--- backend/get_video.py
+++ backend/get_video.py
@@ -1,10 +1,16 @@
 from youtubesearchpython import *
 import json
 
+class CouldNotGetName(Exception):
+    pass
+
 def id_to_name(video_id) -> str:
     vid = Video.getInfo(video_id, mode=ResultMode.json)
 
-    vid = json.loads(vid)
+    try:
+        vid = json.loads(vid)
+    except:
+        raise CouldNotGetName("Video id invalid")
 
     return vid['title']
 
--- backend/server.py
+++ backend/server.py
@@ -3,8 +3,36 @@ from get_transcript import id_to_transcript
 from get_chat import id_to_chat
 from magic import magic
 from get_video import id_to_name
+from chat_downloader import errors as chatErrors
+from chat_downloader.errors import NoChatReplay, ChatDownloaderError
+from youtube_transcript_api._errors import TranscriptsDisabled, CouldNotRetrieveTranscript
+from youtube_transcript_api import _errors as transcriptErrors
+from get_video import CouldNotGetName
 
 APP = Flask(__name__)
+
+class APIError(Exception):
+    pass
+
+class APITranscriptError(APIError):
+    code = 406
+    description = "Transcript error"
+
+class APIChatError(APIError):
+    code = 406
+    description = "Live chat error"
+
+class APIVideoError(APIError):
+    code = 406
+    description = "Video error"
+
+@APP.errorhandler(APIError)
+def handle_exception(err):
+    response = {"error": err.description}
+    response["message"] = "" if len(err.args) == 0 else err.args[0]
+    APP.logger.error(f"{err.description}: {response['message']}")
+    return response, err.code
+
 
 @APP.route("/dummy_transcript", methods=["GET"])
 def get_dummy_transcript():
@@ -32,7 +60,10 @@ def get_transcript_from_id():
 def get_title():
     video_id = request.args.get("id")
 
-    name = id_to_name(video_id)
+    try:
+        name = id_to_name(video_id)
+    except CouldNotGetName:
+        raise APIVideoError("Could not retrieve this video. Please check that the link is correct.")
 
     return {
         "name": name
@@ -42,23 +73,17 @@ def get_title():
 def get_magic():
     video_id = request.args.get("id")
 
-    name = id_to_name(video_id)
-    qna = magic(id_to_transcript(video_id), id_to_chat(video_id))
+    try:
+        qna = magic(id_to_transcript(video_id), id_to_chat(video_id))
+    except TranscriptsDisabled:
+        raise APITranscriptError("Transcripts have been disabled on this video")
+    except CouldNotRetrieveTranscript:
+        raise APITranscriptError("Transcript could not be retrieved from this video")
+    except NoChatReplay:
+        raise APIChatError("This video does not have a chat replay")
+    except ChatDownloaderError:
+        raise APIChatError("Chat could not be retrieved from this video")
 
     return {
-        "name": name,
         "qna": qna
     }
-
-'''
-[
-            {
-                "question": "how do i do a linked list",
-                "answer": "i love linked lists !"
-            },
-            {
-                "question": "when is the exam",
-                "answer": "the exam is tomorrow"
-            }
-        ]
-'''
