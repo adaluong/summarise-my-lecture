574c56b4c308ceabb7223aa2a2d7cfa0b98a7b4b
Angeni Bai <angenibai@gmail.com>
2021-09-09 22:59:49 +1000

    more colour stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 import Result from './pages/Result';
 import Search from './pages/Search';
 //import { Container, Navbar } from 'react-bootstrap';
-import blacklogo from './black_logo_small.png';
+import blacklogo from './black_logo.png';
 import { Navbar, Image } from 'react-bulma-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 const theme = {
   maintext: "#2c2c2c",
   background: "#fffff",
-  
+  accent: "#44d9e6"
 };
 
 
@@ -19,18 +20,20 @@ const App = () => {
   
   return (
     <Router>
-      <Navbar transparent={true}>
-        <Navbar.Brand>
-          <Navbar.Item>
-            <Link to="/">
-              <Image src={blacklogo} alt="summarise my hackathon logo" size="5"></Image>
-            </Link>
-          </Navbar.Item>
-        </Navbar.Brand>
-        
-      </Navbar>
-      <Route exact path='/' component={Search} />
-      <Route exact path='/result/:videoId' component={Result} />
+      <ThemeProvider theme={theme}>
+        <Navbar transparent={true}>
+          <Navbar.Brand>
+            <Navbar.Item>
+              <Link to="/">
+                <img src={blacklogo} alt="summarise my hackathon logo" height="100"></img>
+              </Link>
+            </Navbar.Item>
+          </Navbar.Brand>
+          
+        </Navbar>
+        <Route exact path='/' component={Search} />
+        <Route exact path='/result/:videoId' component={Result} />
+      </ThemeProvider>
     </Router>
   );
 }
